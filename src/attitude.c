5438dbf4f22a46b222b994fe7ce331fd926fd377
David Gianforte <david@gianforte.org>
2015-09-08 19:21:40 -0600

    finishing touches (delayed commit)

--- src/attitude.c
+++ src/attitude.c
@@ -3,9 +3,9 @@
 
 static Window* window;
 static Layer* attitude_layer;
-static TextLayer* text_layer;
 static GRect bounds;
 static int accel_x, accel_y, accel_z;
+static bool light;
 
 // http://forums.getpebble.com/discussion/5792/sqrt-function
 static int32_t my_sqrt(int32_t num) {
@@ -20,10 +20,28 @@ static int32_t my_sqrt(int32_t num) {
   return estimate;
 }
 
+// Draw a line, rotated around the center of the screen by "bank" degrees
+static void draw_absolute_rotated_line(GContext *ctx, int bank, int x1, int y1, int x2, int y2) {
+  int px1, py1, px2, py2;
+  x1 = x1 - 72;
+  y1 = y1 - 84;
+  x2 = x2 - 72;
+  y2 = y2 - 84;
+  px1 = (cos_lookup(-bank) * x1 - sin_lookup(-bank) * y1) / 0x10000;
+  py1 = (sin_lookup(-bank) * x1 + cos_lookup(-bank) * y1) / 0x10000;
+  px2 = (cos_lookup(-bank) * x2 - sin_lookup(-bank) * y2) / 0x10000;
+  py2 = (sin_lookup(-bank) * x2 + cos_lookup(-bank) * y2) / 0x10000;
+  px1 = px1 + 72;
+  py1 = py1 + 84;
+  px2 = px2 + 72;
+  py2 = py2 + 84;
+  graphics_draw_line(ctx, GPoint(px1, py1), GPoint(px2, py2));
+}
+
 static void draw_attitude(Layer *layer, GContext *ctx) {
-  static char s_buffer[128];
   int pitch_scaling = 1000;
 
+  // Calculate pitch and bank angles
   int pitch = my_sqrt((int32_t)accel_x * (int32_t)accel_x + (int32_t)accel_y * (int32_t)accel_y);
   pitch = atan2_lookup(accel_z, pitch);
   if (pitch > 0x8000) {
@@ -72,17 +90,18 @@ static void draw_attitude(Layer *layer, GContext *ctx) {
       }
     }
   }
-  // Draw lines
+
+  // Draw pitch angle lines
   graphics_context_set_stroke_color(ctx, GColorWhite);
-  int x, x2, x3, y, y2, y3, px, px2, px3, py, py2, py3;
+  int x, x2, y, px, px2, py, py2;
   for (i = -26; i < 26; i++) {
     if (i % 2 == 1 || -i % 2 == 1) {
-      x = 10; // 20 pixels
+      x = 10; // 10 pixels
     } else {
       x = 20; // 20 pixels
     }
     x2 = -x;
-    y = 0x10000 * i / 72; // ten degrees
+    y = 0x10000 * i / 72;
     y = (y + pitch)*pitch_scaling / 0x10000;
     px = (cos_lookup(-bank) * x - sin_lookup(-bank) * y) / 0x10000;
     px2 = (cos_lookup(-bank) * x2 - sin_lookup(-bank) * y) / 0x10000;
@@ -113,58 +132,47 @@ static void draw_attitude(Layer *layer, GContext *ctx) {
     snprintf(text, sizeof(text), "%i0", -i);
     graphics_draw_text(ctx, text, fonts_get_system_font(FONT_KEY_GOTHIC_14), GRect(px, py, 20, 10), GTextOverflowModeWordWrap, GTextAlignmentLeft, NULL);
   }
-  // Draw bank indicator
+
+  // Draw the bank indicator
+  // draw the arrow at 0 deg
   graphics_context_set_stroke_color(ctx, GColorWhite);
+  graphics_context_set_stroke_width(ctx, 5);
+  draw_absolute_rotated_line(ctx, bank, 72, 10, 76, 2);
+  draw_absolute_rotated_line(ctx, bank, 72, 10, 68, 2);
+  draw_absolute_rotated_line(ctx, bank, 76, 2, 68, 2);
+  // draw the bank indicator tick marks going left
   graphics_context_set_stroke_width(ctx, 3);
-  // the arrow at 0 deg
-  graphics_draw_line(ctx, GPoint(72, 11), GPoint(76, 3));
-  graphics_draw_line(ctx, GPoint(72, 11), GPoint(68, 3));
-  graphics_draw_line(ctx, GPoint(76, 3), GPoint(68, 3));
-  graphics_context_set_stroke_width(ctx, 1);
-  graphics_draw_line(ctx, GPoint(59, 12), GPoint(58, 4));
-  graphics_draw_line(ctx, GPoint(47, 15), GPoint(44, 7));
-  graphics_draw_line(ctx, GPoint(35, 21), GPoint(31, 14));
-  graphics_draw_line(ctx, GPoint(20, 32), GPoint(16, 24));
-  graphics_draw_line(ctx, GPoint(20, 32), GPoint(12, 28));
-  graphics_draw_line(ctx, GPoint(16, 24), GPoint(12, 28));
-  graphics_draw_line(ctx, GPoint(9, 47), GPoint(2, 43));
-  graphics_draw_line(ctx, GPoint(84, 12), GPoint(85, 4));
-  graphics_draw_line(ctx, GPoint(96, 15), GPoint(99, 7));
-  graphics_draw_line(ctx, GPoint(108, 21), GPoint(112, 14));
-  graphics_draw_line(ctx, GPoint(123, 32), GPoint(127, 24));
-  graphics_draw_line(ctx, GPoint(123, 32), GPoint(131, 28));
-  graphics_draw_line(ctx, GPoint(127, 24), GPoint(131, 28));
-  graphics_draw_line(ctx, GPoint(134, 47), GPoint(141, 43));
+  draw_absolute_rotated_line(ctx, bank, 59, 12, 58, 9);
+  draw_absolute_rotated_line(ctx, bank, 47, 15, 46, 12);
+  draw_absolute_rotated_line(ctx, bank, 35, 21, 31, 14);
+  draw_absolute_rotated_line(ctx, bank, 20, 32, 16, 24);
+  draw_absolute_rotated_line(ctx, bank, 20, 32, 12, 28);
+  draw_absolute_rotated_line(ctx, bank, 16, 24, 12, 28);
+  draw_absolute_rotated_line(ctx, bank, 9, 47, 2, 43);
+  // draw the bank indicator tick marks going right
+  draw_absolute_rotated_line(ctx, bank, 84, 12, 85, 9);
+  draw_absolute_rotated_line(ctx, bank, 96, 15, 97, 12);
+  draw_absolute_rotated_line(ctx, bank, 108, 21, 112, 14);
+  draw_absolute_rotated_line(ctx, bank, 123, 32, 127, 24);
+  draw_absolute_rotated_line(ctx, bank, 123, 32, 131, 28);
+  draw_absolute_rotated_line(ctx, bank, 127, 24, 131, 28);
+  draw_absolute_rotated_line(ctx, bank, 134, 47, 141, 43);
+  // Draw the bank indicator
   graphics_context_set_stroke_color(ctx, GColorYellow);
-  graphics_context_set_stroke_width(ctx, 3);
-  x = 0;
-  y = -72;
-  x2 = 4;
-  y2 = -64;
-  x3 = -x2;
-  y3 = y2;
-  px = (cos_lookup(-bank) * x - sin_lookup(-bank) * y) / 0x10000;
-  py = (sin_lookup(-bank) * x + cos_lookup(-bank) * y) / 0x10000;
-  px2 = (cos_lookup(-bank) * x2 - sin_lookup(-bank) * y2) / 0x10000;
-  py2 = (sin_lookup(-bank) * x2 + cos_lookup(-bank) * y2) / 0x10000;
-  px3 = (cos_lookup(-bank) * x3 - sin_lookup(-bank) * y3) / 0x10000;
-  py3 = (sin_lookup(-bank) * x3 + cos_lookup(-bank) * y3) / 0x10000;
-  px = px + 72;
-  py = py + 84;
-  px2 = px2 + 72;
-  py2 = py2 + 84;
-  px3 = px3 + 72;
-  py3 = py3 + 84;
-  graphics_draw_line(ctx, GPoint(px, py), GPoint(px2, py2));
-  graphics_draw_line(ctx, GPoint(px2, py2), GPoint(px3, py3));
-  graphics_draw_line(ctx, GPoint(px3, py3), GPoint(px, py));
+  graphics_context_set_stroke_width(ctx, 5);
+  graphics_draw_line(ctx, GPoint(72, 16), GPoint(76, 24));
+  graphics_draw_line(ctx, GPoint(72, 16), GPoint(68, 24));
+  graphics_draw_line(ctx, GPoint(76, 24), GPoint(68, 24));
+
   // Draw crosshair
-  graphics_context_set_stroke_color(ctx, GColorBlack);
+  graphics_context_set_stroke_color(ctx, GColorYellow);
   graphics_context_set_stroke_width(ctx, 5);
   graphics_draw_line(ctx, GPoint(72, 84), GPoint(72, 84));
   graphics_context_set_stroke_width(ctx, 3);
   graphics_draw_line(ctx, GPoint(37, 84), GPoint(57, 84));
+  graphics_draw_line(ctx, GPoint(57, 84), GPoint(57, 88));
   graphics_draw_line(ctx, GPoint(87, 84), GPoint(109, 84));
+  graphics_draw_line(ctx, GPoint(87, 84), GPoint(87, 88));
 }
 
 static void data_handler(AccelData *data, uint32_t num_samples) {
@@ -175,6 +183,21 @@ static void data_handler(AccelData *data, uint32_t num_samples) {
   layer_mark_dirty(attitude_layer);
 }
 
+static void toggle_light() {
+  light = !light;
+  light_enable(light);
+}
+
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) { toggle_light(); }
+static void up_click_handler(ClickRecognizerRef recognizer, void *context) { toggle_light(); }
+static void down_click_handler(ClickRecognizerRef recognizer, void *context) { toggle_light(); }
+
+static void click_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+}
+
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   bounds = layer_get_bounds(window_layer);
@@ -182,17 +205,10 @@ static void window_load(Window *window) {
   attitude_layer = layer_create(bounds);
   layer_set_update_proc(attitude_layer, draw_attitude);
   layer_add_child(window_layer, attitude_layer);
-
-  /*
-  text_layer = text_layer_create((GRect) { .origin = { 0, 0 }, .size = { bounds.size.w, 100 } });
-  text_layer_set_text(text_layer, "data");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
-  */
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+  layer_destroy(attitude_layer);
 }
 
 static void init(void) {
@@ -203,12 +219,15 @@ static void init(void) {
   });
   const bool animated = true;
   window_stack_push(window, animated);
+  window_set_click_config_provider(window, click_config_provider);
 
   accel_data_service_subscribe(1, data_handler);
-  light_enable(true);
+  light = true;
+  light_enable(light);
 }
 
 static void deinit(void) {
+  accel_data_service_unsubscribe();
   window_destroy(window);
 }
 
